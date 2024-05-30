4401736099bd2f737dcd2e9869bdc9bb57487cc1
stalinsangucho <stalinsangucho87@gmail.com>
2024-05-30 08:09:15 -0500

    Clase Movil

--- src/Procesadores/Procesadores_moviles.java
+++ src/Procesadores/Procesadores_moviles.java
@@ -1,4 +1,31 @@
-package Proceadores;
+package Procesadores;
 
-public class Procesadores_moviles {
+import Procesadores.Procesador;
+
+public class Procesadores_moviles extends Procesador {
+    boolean optimizacion_energia;
+
+    public Procesadores_moviles(){}
+
+
+    public Procesadores_moviles(int nucleos, double consumo, double velocidad, String nombre, boolean optimizacion_energia) {
+        super(nucleos, consumo, velocidad, nombre);
+        this.optimizacion_energia = optimizacion_energia;
+    }
+
+    public boolean isOptimizacion_energia() {
+        return optimizacion_energia;
+    }
+
+    public void setOptimizacion_energia(boolean optimizacion_energia) {
+        this.optimizacion_energia = optimizacion_energia;
+    }
+
+    //metodos Personalizados
+
+    public void imprimirDatosMoviles(){
+        System.out.println("Optimizacion de energia : " + isOptimizacion_energia());
+        System.out.println(this.optimizacion_energia);
+
+    }
 }
