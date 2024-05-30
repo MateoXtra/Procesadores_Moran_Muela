d75f56011f73a243918dd7534063ba1674a7e353
stalinsangucho <stalinsangucho87@gmail.com>
2024-05-30 08:20:12 -0500

    Prueba1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import Procesadores.Procesador;
 
 public class Main {
     public static void main(String[] args) {
-        Procesador procesador = new Procesador();
+        Procesador procesador = new Procesador(4,12.232,12.343,"Intel Ik98455");
+
+        procesador.impromirDatos();
+
     }
 
 }
