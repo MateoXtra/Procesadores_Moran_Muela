17a7197814d67c6356f768210f81f5ab3297b521
mateoxtra <mateoextra93@gmail.com>
2024-05-30 08:02:18 -0500

    agrego clase procesadores escritorio

---
+++ src/Procesadores/Procesadores_Escritorio.java
@@ -0,0 +1,35 @@
+package Procesadores;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Procesadores_Escritorio extends Procesador {
+    String socket;
+
+    public Procesadores_Escritorio() {
+    }
+
+    public Procesadores_Escritorio(int nucleos, double consumo, double velocidad, String nombre, String socket) {
+        super(nucleos, consumo, velocidad, nombre);
+        this.socket = socket;
+    }
+
+    public String getSocket() {
+        return socket;
+    }
+
+    public void setSocket(String socket) {
+        this.socket = socket;
+    }
+
+    //metodos Personalizados
+
+    public void imprimirDatosEsctirotio(){
+        System.out.println("Socket: " + socket);
+        System.out.println(this.socket);
+
+    }
+}
+
+
+
+
