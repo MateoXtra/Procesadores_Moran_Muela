d446a85e5287dc68a9c5e2b9b7a8044f62430de0
mateoxtra <mateoextra93@gmail.com>
2024-05-30 08:45:37 -0500

    imprimo datos

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,10 @@ public class Main {
 
         Procesadores_Amd procesador_amd = new Procesadores_Amd();
         Procesadores_Intel procesador_intel = new Procesadores_Intel(8,3.24,5.65,"Intel 3-7100","Intel Socket 1151",false);
+        procesador_intel.imprimirDatos();
+        procesador_intel.ImprimirdatosGraficos();
+        procesador_amd.imprimirDatos();
+        procesador_amd.imprimirDatosAmd();
     }
 
 }
