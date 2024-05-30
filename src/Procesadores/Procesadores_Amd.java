ba949623300d57234c71b1887f96c60db0e11260
mateoxtra <mateoextra93@gmail.com>
2024-05-30 08:07:01 -0500

    agrego clase procesadores amd

---
+++ src/Procesadores/Procesadores_Amd.java
@@ -0,0 +1,25 @@
+package Procesadores;
+
+public class Procesadores_Amd extends Procesadores_Escritorio{
+    boolean sensor_Temp;
+
+    public Procesadores_Amd() {
+    }
+
+    public Procesadores_Amd(int nucleos, double consumo, double velocidad, String nombre, String socket, boolean sensor_Temp) {
+        super(nucleos, consumo, velocidad, nombre, socket);
+        this.sensor_Temp = sensor_Temp;
+    }
+
+    public boolean isSensor_Temp() {
+        return sensor_Temp;
+    }
+
+    public void setSensor_Temp(boolean sensor_Temp) {
+        this.sensor_Temp = sensor_Temp;
+    }
+
+    //metodos
+    public void imprimirDatosAmd(){
+        System.out.println("El sensor de temperatura es: " + sensor_Temp);}
+}
